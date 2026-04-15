40684d8a2b634d213f2f901d30f9b2752bde8b99
mhnofficial <nickygomez.29@icloud.com>
2026-04-15 18:27:28 -0400

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ADD THIS LINE BELOW
+  base: "/fictional-octo-carnival/", 
   server: {
     host: "::",
     port: 8080,

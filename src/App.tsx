7882a75513707c67768685fda9de4ad7834a2ae6
mhnofficial <nickygomez.29@icloud.com>
2026-04-15 18:36:58 -0400

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+        <BrowserRouter basename="/fictional-octo-carnival">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
